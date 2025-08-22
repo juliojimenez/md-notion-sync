61155e63fd655ae1fdf7cbc315076d2e89c63f7d
Julio Jimenez <julio@clickhouse.com>
2025-08-22 11:50:24 -0400

    chore(jest): Setup
    Signed-off-by: Julio Jimenez <julio@clickhouse.com>

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+// Test setup file
+import { jest } from '@jest/globals';
+
+// Mock console methods to reduce noise in tests
+global.console = {
+  ...console,
+  log: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+};
