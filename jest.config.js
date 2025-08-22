4529a5ee45ae4e988725b0f6127bb0eeefbd4438
Julio Jimenez <julio@clickhouse.com>
2025-08-22 11:42:41 -0400

    chore(jest): Config (#9)
    Signed-off-by: Julio Jimenez <julio@clickhouse.com>

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/*.(test|spec).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/cli.ts' // Exclude CLI from coverage as it's harder to test
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'text',
+    'lcov',
+    'html'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testTimeout: 10000
+};
